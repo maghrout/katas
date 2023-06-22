1fe36c570bcc19ea3d01c1f041eafb3b32e263c6
maghrout <maghrout@googlemail.com>
2023-06-22 16:22:03 +0100

    TICTACTOE

--- tictactoe-001-other-tom/test/tictactoe.test.ts
+++ tictactoe-001-other-tom/test/tictactoe.test.ts
@@ -1,5 +1,48 @@
-describe('Default test', () => {
-    it('to pass', () => {
-        expect(true).toBe(true)
+class Game {
+    private board = {
+        topLeft: '',
+        topMiddle: '',
+        topRight: '',
+        left: '',
+        middle: '',
+        right: '',
+        bottomLeft: '',
+        bottomMiddle: '',
+        bottomRight: ''
+    };
+
+    placePiece(positionOnBoard: string) {
+        if (positionOnBoard === 'topMiddle') {
+            this.board.topMiddle = 'X';
+        }
+        else if (positionOnBoard === 'topRight') {
+            this.board.topRight = 'O';
+        }
+        else {
+            this.board.topLeft = 'O';
+        }
+    }
+
+    currentStateOfBoard() {
+        return this.board;
+    }
+}
+
+describe('Given an empty board and the first player is O when they place an O in the topLeft square', () => {
+    it('should place a counter on the topLeft square', () => {
+        const game = new Game();
+        game.placePiece('topLeft');
+        const currentBoardState = game.currentStateOfBoard();
+        expect(currentBoardState).toEqual({
+            topLeft: 'O',
+            topMiddle: '',
+            topRight: '',
+            left: '',
+            middle: '',
+            right: '',
+            bottomLeft: '',
+            bottomMiddle: '',
+            bottomRight: ''
+        });
     })
 })
